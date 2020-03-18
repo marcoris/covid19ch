784cf4d664a792170a9a212a867e0733f99e5293
Marco Ris <marco.ris@risdesign.ch>
2020-03-18 21:47:58 +0100

    feat: Add calculation and progressbar

--- js/app.js
+++ js/app.js
@@ -8,11 +8,20 @@ xhttp.onreadystatechange = function() {
         var confirmed = data.confirmed;
         var recovered = data.recovered;
         var deaths = data.deaths;
+        var swissPopulation = 8570000;
+        var infectedWidth = 100 / swissPopulation * (confirmed + recovered + deaths) + 10;
+        var populationWidth = 100 - infectedWidth;
 
         // Display data
         document.getElementById("confirmed").innerHTML = confirmed;
         document.getElementById("recovered").innerHTML = recovered;
         document.getElementById("deaths").innerHTML = deaths;
+        document.getElementById("population").style.width = populationWidth + '%';
+        document.getElementById("population").innerHTML = populationWidth.toFixed(2) + '%';
+        document.getElementById("infected").style.width = infectedWidth + '%';
+        if (infectedWidth > 15) {
+            document.getElementById("infected").innerHTML = infectedWidth.toFixed(2) + '%';
+        }
     }
 };
 xhttp.open("GET", "https://covid19.mathdro.id/api/countries/ch/recovered", true);
