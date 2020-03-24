930435b55d627df2576068d21fe16e0d7f362097
Marco Ris <ris@contria.com>
2020-03-24 06:55:59 +0100

    fix(js): Check if data is set and handle it if not

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         // Set response data
         var data = JSON.parse(xhttp.responseText)[0];
-        var confirmed = data.confirmed;
-        var recovered = data.recovered;
-        var deaths = data.deaths;
+        console.log(data);
+        var confirmed = data ? data.confirmed : 0;
+        var recovered = data ? data.recovered : 0;
+        var deaths = data ? data.deaths : 0;
         var swissPopulation = 8570000;
         var infectedWidth = 100 / swissPopulation * (confirmed + recovered + deaths);
         var populationWidth = 100 - infectedWidth;
@@ -16,52 +17,60 @@ xhttp.onreadystatechange = function() {
         var currentDeaths = 0;
         var currentPopulation = 0;
 
-        // Confirmed
-        var timerConfirmed = setInterval(function() {
-            currentConfirmed += 1;
-            document.getElementById("confirmed").innerHTML = currentConfirmed;
-            if (currentConfirmed >= 500) {
-                document.getElementById("confirmed").innerHTML = confirmed;
-                clearInterval(timerConfirmed);
-            }
-        }, 0);
-        
-        // Recovered
-        var timerRecovered = setInterval(function() {
-            currentRecovered += 1;
-            document.getElementById("recovered").innerHTML = currentRecovered;
-            if (currentRecovered == recovered) {
-                clearInterval(timerRecovered);
-            }
-        }, 0);
-        
-        // Deaths
-        var timerDeaths = setInterval(function() {
-            currentDeaths += 1;
-            document.getElementById("deaths").innerHTML = currentDeaths;
-            if (currentDeaths == deaths) {
-                clearInterval(timerDeaths);
-            }
-        }, 0);
-
-        document.getElementById("population").style.width = populationWidth + '%';
-        document.getElementById("infected").style.width = infectedWidth + '%';
-        
-        // Show population percentage when there is enought space
-        if (populationWidth > 15) {
-            var timerPopulation = setInterval(function() {
-                currentPopulation += 0.1;
-                document.getElementById("population").innerHTML = currentPopulation.toFixed(2) + '%';
-                if (currentPopulation >= 30) {
-                    document.getElementById("population").innerHTML = populationWidth.toFixed(2) + '%';
-                    clearInterval(timerPopulation);
+        if (data) {
+            // Confirmed
+            var timerConfirmed = setInterval(function() {
+                currentConfirmed += 1;
+                document.getElementById("confirmed").innerHTML = currentConfirmed;
+                if (currentConfirmed >= 500) {
+                    document.getElementById("confirmed").innerHTML = confirmed;
+                    clearInterval(timerConfirmed);
                 }
             }, 0);
-        }
-
-        // Show infected percentage when there is enought space
-        if (infectedWidth > 15) {
-            document.getElementById("infected").innerHTML = infectedWidth.toFixed(2) + '%';
+            
+            // Recovered
+            var timerRecovered = setInterval(function() {
+                currentRecovered += 1;
+                document.getElementById("recovered").innerHTML = currentRecovered;
+                if (currentRecovered == recovered) {
+                    clearInterval(timerRecovered);
+                }
+            }, 0);
+            
+            // Deaths
+            var timerDeaths = setInterval(function() {
+                currentDeaths += 1;
+                document.getElementById("deaths").innerHTML = currentDeaths;
+                if (currentDeaths == deaths) {
+                    clearInterval(timerDeaths);
+                }
+            }, 0);
+    
+            document.getElementById("population").style.width = populationWidth + '%';
+            document.getElementById("infected").style.width = infectedWidth + '%';
+            
+            // Show population percentage when there is enought space
+            if (populationWidth > 15) {
+                var timerPopulation = setInterval(function() {
+                    currentPopulation += 0.1;
+                    document.getElementById("population").innerHTML = currentPopulation.toFixed(2) + '%';
+                    if (currentPopulation >= 30) {
+                        document.getElementById("population").innerHTML = populationWidth.toFixed(2) + '%';
+                        clearInterval(timerPopulation);
+                    }
+                }, 0);
+            }
+    
+            // Show infected percentage when there is enought space
+            if (infectedWidth > 15) {
+                document.getElementById("infected").innerHTML = infectedWidth.toFixed(2) + '%';
+            }
+        } else {
+            document.getElementById("confirmed").innerHTML = 0;
+            document.getElementById("recovered").innerHTML = 0;
+            document.getElementById("deaths").innerHTML = currentDeaths;
+            document.getElementById("population").style.width = '100%';
+            document.getElementById("population").innerHTML = '100%';
         }
     }
 };
