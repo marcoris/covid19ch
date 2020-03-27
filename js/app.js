7fb302ece68f2cb280f49a4286810a5bb4ebd111
Marco Ris <ris@contria.com>
2020-03-27 14:47:50 +0100

    feat(js): Format numbers in correct CH format

--- js/app.js
+++ js/app.js
@@ -23,32 +23,32 @@ xhttp.onreadystatechange = function() {
                 currentConfirmed += 1;
                 document.getElementById("confirmed").innerHTML = currentConfirmed;
                 if (currentConfirmed >= 500) {
-                    document.getElementById("confirmed").innerHTML = confirmed;
+                    document.getElementById("confirmed").innerHTML = new Intl.NumberFormat('de-CH').format(confirmed);
                     clearInterval(timerConfirmed);
                 }
             }, 0);
-            
+
             // Recovered
             var timerRecovered = setInterval(function() {
                 currentRecovered += 1;
-                document.getElementById("recovered").innerHTML = currentRecovered;
+                document.getElementById("recovered").innerHTML = new Intl.NumberFormat('de-CH').format(currentRecovered);
                 if (currentRecovered == recovered) {
                     clearInterval(timerRecovered);
                 }
             }, 0);
-            
+
             // Deaths
             var timerDeaths = setInterval(function() {
                 currentDeaths += 1;
-                document.getElementById("deaths").innerHTML = currentDeaths;
+                document.getElementById("deaths").innerHTML = new Intl.NumberFormat('de-CH').format(currentDeaths);
                 if (currentDeaths == deaths) {
                     clearInterval(timerDeaths);
                 }
             }, 0);
-    
+
             document.getElementById("population").style.width = populationWidth + '%';
             document.getElementById("infected").style.width = infectedWidth + '%';
-            
+
             // Show population percentage when there is enought space
             if (populationWidth > 15) {
                 var timerPopulation = setInterval(function() {
@@ -60,7 +60,7 @@ xhttp.onreadystatechange = function() {
                     }
                 }, 0);
             }
-    
+
             // Show infected percentage when there is enought space
             if (infectedWidth > 15) {
                 document.getElementById("infected").innerHTML = infectedWidth.toFixed(2) + '%';
